9ad64952c1a65a18fa25e315fc8e16148c9229f3
Jeffryt Rubiano <jeffryt.rubiano@globant.com>
2019-04-08 16:30:38 -0500

    Change project name Shop.UIClassic.Android to Shop.UIClassic.A

--- source/Shop.UIClassic.A/Properties/AssemblyInfo.cs
+++ source/Shop.UIClassic.A/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shop.UIClassic.Android")]
+[assembly: AssemblyTitle("Shop.UIClassic.A")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shop.UIClassic.Android")]
+[assembly: AssemblyProduct("Shop.UIClassic.A")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
